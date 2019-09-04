1aa17a315fdeaabbe462c540c2307abc8e218ccf
Baniewicz <baniewiczd@media-saturn.com>
2019-09-04 09:21:45 +0200

    Style started

---
+++ guessTheWord.js
@@ -0,0 +1,14 @@
+const words = ["Regenschirm", "Sonnenbrille", "Tierheim"];
+    
+console.log(words);
+
+class GuessTheWordGame {
+    constructor(words){
+        this.words = words;
+        this.pickedWord = "";
+    }
+}
+
+//var index = Math.floor(Math.random()*this.words.length);
+//const wordToGuess = this.words[index];
+//const wordLength = wordToGuess.length;
